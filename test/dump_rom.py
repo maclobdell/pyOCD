0e04b9e41951683afe3f330a072c495d2125f95a
maclobdell <maclain.lobdell@arm.com>
2016-02-23 14:28:18 -0600

    change flash address start to 0x2000

--- test/dump_rom.py
+++ test/dump_rom.py
@@ -2,7 +2,7 @@ from pyOCD.board import MbedBoard
 
 with MbedBoard.chooseBoard() as board:
     target = board.target
-    data = target.readBlockMemoryUnaligned8(0x3000, 32 * 1024)
+    data = target.readBlockMemoryUnaligned8(0x2000, 32 * 1024)
     data = bytearray(data)
     f = open("dump.bin", "wb")
     f.write(data)
